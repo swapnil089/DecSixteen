1053d351e3af1d6e79c78201c49361cab416e3e6
swapnil089 <swapnilastikar41@gmail.com>
2023-12-16 13:47:34 +0530

    this the try to commit first branch

--- src/com/wavityandziontech/One.java
+++ src/com/wavityandziontech/One.java
@@ -5,6 +5,7 @@ public class One {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("This the first git tutorials");
+		System.out.println("this the second commit");
 	}
 
 }

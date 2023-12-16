8b3dea75fa6f0eb8aec0eb3ea22b57c294752016
swapnil089 <swapnilastikar41@gmail.com>
2023-12-16 13:26:16 +0530

    this is first commit

---
+++ src/com/wavityandziontech/One.java
@@ -0,0 +1,10 @@
+package com.wavityandziontech;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("This the first git tutorials");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module DecGitTutorails {
+}
